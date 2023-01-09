ae3da2b6d27fdf9b30d68fdaffb05d78c0078c8e
Bartix95 <116849167+Bartix95@users.noreply.github.com>
2023-01-09 13:57:42 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Grocki
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            string l1 = Label1.Text;
+            string l2 = Label2.Text;
+            string l3 = Label3.Text;
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                var utcTime = DateTime.UtcNow.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+                var hongKongTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Hongkong").ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+                var localTime = DateTime.Now.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+
+                Label1.Text = l1 + utcTime;
+                Label2.Text = l2 + hongKongTime;
+                Label3.Text = l3 + localTime;
+                return true;
+            });
+            
+        }
+
+        
+    }
+}
